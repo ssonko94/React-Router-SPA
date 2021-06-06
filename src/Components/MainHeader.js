e43974dc904f260144048d7e68c4419836cc703b
ssonko94 <ssonkojimmy@gmail.com>
2021-06-06 20:07:52 +0300

    use a nav link to get links that a specific for navigation

--- src/Components/MainHeader.js
+++ src/Components/MainHeader.js
@@ -1,16 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import classes from "./MainHeader.module.css";
 
 const MainHeader = () => {
   return (
-    <header>
+    <header className={classes.header}>
       <nav>
         <ul>
           <li>
-            <Link to="/welcome">Welcome</Link>
+            <NavLink to="/welcome">Welcome</NavLink>
           </li>
           <li>
-            <Link to="/products">Products</Link>
+            <NavLink to="/products">Products</NavLink>
           </li>
         </ul>
       </nav>
