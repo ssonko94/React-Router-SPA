0f7293bb1ccdc84097aa64957293285dbb7c7ae9
ssonko94 <ssonkojimmy@gmail.com>
2021-06-06 21:27:09 +0300

    use params to access add optional parameters to the link

---
+++ src/pages/ProductDetail.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {useParams} from 'react-router-dom'
+
+const ProductDetail = () => {
+    const params = useParams()
+  return (
+    <section>
+      <h1>Product Details</h1>
+    </section>
+  );
+};
+
+export default ProductDetail;
