69cf6af835a64c724cbba4ec350f90804e1a4ff2
ssonko94 <ssonkojimmy@gmail.com>
2021-06-06 21:23:48 +0300

    add a nested path to the welcome page

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import { Route } from "react-router-dom";
 
 const Welcome = () => {
-    return (
-        <h1>
-          The Welcome Page  
-        </h1>
-    )
-}
+  return (
+    <section>
+      <h1>The Welcome Page </h1>
+      <Route path="/welcome/new-user">
+        <p>Welcome new user 😊</p>
+      </Route>
+    </section>
+  );
+};
 
-export default Welcome
+export default Welcome;
