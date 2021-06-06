996c0a1cf8a32e83146a6490ddde0eefb437bbd5
ssonko94 <ssonkojimmy@gmail.com>
2021-06-06 20:02:59 +0300

    add route components with paths setting them to the path

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { Route } from "react-router-dom";
+
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import MainHeader from "./Components/MainHeader";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <header>
+        <MainHeader />
+      </header>
+      <main>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+      </main>
     </div>
   );
 }
