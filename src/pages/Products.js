a29d7cbb24fac6782e2cce54ab249fbb7fb51d90
ssonko94 <ssonkojimmy@gmail.com>
2021-06-06 21:03:02 +0300

    wrap the products with links to  show details of a product

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Products = () => {
   return (
-    <h1>
-      The products Page
-    </h1>
-  )
-}
+    <section>
+      <h1>The products Page</h1>
+      <ul>
+        <li>
+          <Link to="/products/p1">A Book</Link>
+        </li>
+        <li>
+          <Link to="/products/p2">A Carpet</Link>
+        </li>
+        <li>
+          <Link to="/products/p3">A Phone</Link>
+        </li>
+      </ul>
+    </section>
+  );
+};
 
-export default Products
+export default Products;
